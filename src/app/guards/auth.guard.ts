b64ff8f67a8785d206d4d373ac9b91bccfc57214
David Luis Wiegandt <David-Luis.Wiegandt@adesso.de>
2023-07-06 01:21:51 +0200

    updated to Angular CLI 16

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services';
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
     constructor(private authService: AuthService, private snackBar: MatSnackBar) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
